b4809bf0e0d677e6c9b3849cfadc5299246a0f20
boombeamw <95232764+boombeamw@users.noreply.github.com>
2023-11-06 14:09:06 +0700

    Update 1_WC415_HomePage_Rev01.py

--- 1_WC415_HomePage_Rev01.py
+++ 1_WC415_HomePage_Rev01.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 #import numpy as np
 import pickle
-import plotly.express as px
+#import plotly.express as px
 import xgboost as xgb
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
